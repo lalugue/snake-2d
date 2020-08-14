4e68980c5b379f856d76f4f303bb605395ad447f
lalugue <51893731+lalugue@users.noreply.github.com>
2020-08-14 09:20:37 +0800

    add initial food spawning

---
+++ Assets/Food.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    /*
+    x: -5.5 to 4.5
+    y: -3.5 to 3.5
+    */
+    // Start is called before the first frame update
+    float x;
+    float y;
+
+    System.Random rnd = new System.Random();
+    
+    
+    void Start()
+    {
+        this.transform.position = new Vector2((float)rnd.Next(-5,6) - 0.5f, (float)rnd.Next(-3,5) - 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
