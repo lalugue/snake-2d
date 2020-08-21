419e176b71b312ccf23f762a50f0a7f9b6a3a599
lalugue <51893731+lalugue@users.noreply.github.com>
2020-08-21 09:26:55 +0800

    add scoring mechanism

---
+++ Assets/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int score = 0;
+    public Text text;
+    void Start()
+    {
+        text.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(){
+        score++;
+        text.text = score.ToString();
+    }
+}
