491f6f3feb02f3553503d7d969bde5648f2caaac
lalugue <51893731+lalugue@users.noreply.github.com>
2020-08-23 00:40:02 +0800

    update food respawn mechanism

--- Assets/Food.cs
+++ Assets/Food.cs
@@ -15,6 +15,8 @@ public class Food : MonoBehaviour
     float x;
     float y;
 
+    public GameObject foodObject;
+
        
     
     void Start()
@@ -22,7 +24,27 @@ public class Food : MonoBehaviour
         x = (float)rnd.Next(-5,6) - 0.5f;
         y = (float)rnd.Next(-3,5) - 0.5f;
 
+        Collider2D[] intersecting = Physics2D.OverlapCircleAll(new Vector2(x, y), 0.01f);
+        while(intersecting.Length > 0){
+            Debug.Log("food spawn at snake body detected");
+            x = (float)rnd.Next(-5,6) - 0.5f;
+            y = (float)rnd.Next(-3,5) - 0.5f;
+            intersecting = Physics2D.OverlapCircleAll(new Vector2(x, y), 0.01f);
+        }
+
         this.transform.position = new Vector2(x, y);
+
+        /*if (intersecting.Length == 0) {
+            //code to run if nothing is intersecting as the length is 0
+            this.transform.position = new Vector2(x, y);
+        } else {
+            //code to run if something is intersecting it
+            Debug.Log("Food about to spawn at snake");            
+            Instantiate(foodObject);
+            Destroy(this.gameObject);
+        }*/
+        
+
     }
 
     // Update is called once per frame
