9cb2d5e01153923fc553521615c4819e50d4ddf6
lalugue <51893731+lalugue@users.noreply.github.com>
2020-09-22 21:55:06 +0800

    add scene loading script for button

---
+++ Assets/ButtonLoadScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLoadScene : MonoBehaviour
+{
+      
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+    void OnClick(){
+        Debug.Log("Loading game!");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadGame(){
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
